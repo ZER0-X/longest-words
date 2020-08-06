f5a81874486d262863a505d63e0821e2bc8a38fb
ZER0 <65136727+ZER0-X@users.noreply.github.com>
2020-08-06 11:18:46 +0300

    upload Project

---
+++ longest_words.py
@@ -0,0 +1,36 @@
+###input from a file
+words_list = open(str(input("file_path:"))).read().split()
+
+###input from keyboard
+# words_list = input("type:").split()
+
+max_length = len(max(words_list))
+
+longest_words = []
+
+for word in words_list:
+    if len(word) == max_length:
+        longest_words.append(word)
+
+longest_words_not_repeated = set(longest_words)
+
+
+print(f"""
+max length = {max_length}
+""")
+
+for word in longest_words_not_repeated:
+    repeated = longest_words.count(word)
+    
+    if repeated == 1 :
+        print(f"---->'{word}'")
+        
+    elif repeated != 1 :
+        print(f"---->'{word}' [{repeated}]")
+        
+    print("")
+
+
+print(f"""
+
+The max length is {max_length} Letters""")
